0e115e43d7c3716e1f7c29e986dcff4930c8b0b7
Mahbub Munna <mahbubhassanmunna@gmail.com>
2019-02-26 19:00:40 +0600

    login implemented

--- app/src/main/java/me/moonss/to_dolist/MainActivity.java
+++ app/src/main/java/me/moonss/to_dolist/MainActivity.java
@@ -1,51 +1,67 @@
 package me.moonss.to_dolist;
 
-import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import me.moonss.to_dolist.databinding.ActivityMainBinding;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final int RC_SIGN_IN = 100; //code for checking right onActivityResult
+    private ActivityMainBinding mBinding;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        fireSignUp();
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 
-    public void saveDataToFireStore(View view) {
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
+    //Start Login Activity (Firebase AuthUI)
+    private void fireSignUp() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder()
+                        .build());
 
-// Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.AppTheme)
+                        .setLogo(R.drawable.to_d0_list_black_144dp)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    //Getting Login information from AuthUi Login
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(
+                        this,
+                        "Logged in, user: " +user.getDisplayName(),
+                        Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+        }
     }
 }
